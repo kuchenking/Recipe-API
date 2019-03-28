eda4ea39d130eff9ef15ec0b83106084ef75f6d1
kuchenking <pandukuchen@gmail.com>
2019-03-27 22:49:42 -0700

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -4,7 +4,7 @@ from datetime import datetime
 endpoint #this is the part where the url is
 
 def addRecipe(name, date_added, date_modified, ingredients = "no ingredients set", instructions = " no instructions set", serving_size = " no serving size set", 
-	category = "no category set", notes = "no notes set", )
+	category = "no category set", notes = "no notes set")
 	currentRecipe = {}
 	currentRecipe["name"]=name
 	currentRecipe["ingredients"] = ingredients
@@ -15,37 +15,49 @@ def addRecipe(name, date_added, date_modified, ingredients = "no ingredients set
 	currentRecipe["date_added"] = str(datetime.now())
 	currentRecipe["date_modified"] = str(datetime.now())
 	r = req.post(endpoint, params = currentRecipe)
-	resnum = r.status_code
-	if(resnum >= 400)
-		print("Error")
-	if(resnum >= 300)
-		#something here, what is a redirect anyway
-		print("page moved")
-	if(resnum >=200)
-		print("Success")
+	checkCode(r)
 
 def deleteRecipe(name)
 	r = req.delete(endpoint, name)
+	checkCode(r)
 
 def listRecipies()
 	r = req.get(endpoint, )#i dont know what goes here, never used get before
+	checkCode(r)
+	
 
 def setIngredients(name, new_ing)
 	r = req.put(endpoing, params = {"name":name, "ingredients":new_ing, "date_modified":str(datetime.now())})
+	checkCode(r)
+	
 
 def setInstructions(name, new_ins)
 	r = req.put(endpoing, params = {"name":name, "instructions":new_ins, "date_modified":str(datetime.now())})
+	checkCode(r)
+	
 
 def setServingSize(name, new_ser)
 	r = req.put(endpoing, params = {"name":name, "ingredients":new_ing, "date_modified":str(datetime.now())})
+	checkCode(r)
+	
 
 def setCategory(name, new_cat)
 	r = req.put(endpoing, params = {"name":name, "category":new_cat, "date_modified":str(datetime.now())})
+	checkCode(r)
+	
 
 def setNotes(name, new_not)
 	r = req.put(endpoing, params = {"name":name, "notes":new_not, "date_modified":str(datetime.now())})
-
-
+	checkCode(r)
+	
+def checkCode(code)
+	resnum = code.status_code
+	if(resnum >= 400)
+		print("Error")
+	if(resnum >= 300)
+		print("page moved")
+	if(resnum >=200)
+		print("Success")
 	
 #ingredients
 #instructions
