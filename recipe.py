6ae670818a510b68ce987921168bff3aa871f066
kuchenking <pandukuchen@gmail.com>
2019-03-27 22:58:02 -0700

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -18,34 +18,33 @@ def addRecipe(name, ingredients = "no ingredients set", instructions = " no inst
 	checkCode(r)
 
 def deleteRecipe(name)
-	r = req.delete(endpoint, name)
+	r = req.delete(endpoint, {"name":name})
 	checkCode(r)
 
 def listRecipies()
-	r = req.get(endpoint, )#i dont know what goes here, never used get before
+	r = req.get(endpoint)
 	checkCode(r)
-	
+
+def listRecipes(name)
+	r = req.get(endpoint, {"name":name})
+	checkCode(r)
 
 def setIngredients(name, new_ing)
 	r = req.put(endpoing, params = {"name":name, "ingredients":new_ing, "date_modified":str(datetime.now())})
 	checkCode(r)
 	
-
 def setInstructions(name, new_ins)
 	r = req.put(endpoing, params = {"name":name, "instructions":new_ins, "date_modified":str(datetime.now())})
 	checkCode(r)
 	
-
 def setServingSize(name, new_ser)
 	r = req.put(endpoing, params = {"name":name, "ingredients":new_ing, "date_modified":str(datetime.now())})
 	checkCode(r)
 	
-
 def setCategory(name, new_cat)
 	r = req.put(endpoing, params = {"name":name, "category":new_cat, "date_modified":str(datetime.now())})
 	checkCode(r)
 	
-
 def setNotes(name, new_not)
 	r = req.put(endpoing, params = {"name":name, "notes":new_not, "date_modified":str(datetime.now())})
 	checkCode(r)
